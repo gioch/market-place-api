50e0960b279d5112617bdcdf8de8f046860f0314
George Chkhvirkia <georgechkhvirkia@gmail.com>
2016-02-14 04:16:27 +0400

    Revert " kuwu test commit"
    This reverts commit 6125af8be07617de6161378cdfb3e0dd4046aec8.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,2 @@
 Rails.application.routes.draw do
-
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-
-    scope module: :v1 do
-      # get 'test', to: 'shugar#test'
-    end
-
-  end
-
 end
